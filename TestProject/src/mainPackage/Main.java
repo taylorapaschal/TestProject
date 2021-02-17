34fc86e67c71cc51b4b47ede2d1d7f58ae3dc6e5
taylorapaschal <taylorapaschal@gmail.com>
2021-02-17 16:16:26 -0500

    test

--- TestProject/src/mainPackage/Main.java
+++ TestProject/src/mainPackage/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String eventName = "TO";
+		String eventName = "TO2";
 		System.out.println(eventName);
 
 	}

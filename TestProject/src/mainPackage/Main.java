fa93232d26f266afe1e23c2e2a0d0d6d59f33ffa
taylorapaschal <taylorapaschal@gmail.com>
2021-02-17 16:05:07 -0500

    Changed TechOlympics to TO from issue #2

--- TestProject/src/mainPackage/Main.java
+++ TestProject/src/mainPackage/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String eventName = "TechOlympics";
+		String eventName = "TO";
 		System.out.println(eventName);
 
 	}
